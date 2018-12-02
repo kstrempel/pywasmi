35993ac0d526410d4e0967ffa1628ac6761b4420
Kai Strempel <kstrempel@gmail.com>
2018-12-02 15:42:34 +0100

    rust-format

--- src/instance.rs
+++ src/instance.rs
@@ -1,14 +1,13 @@
-use wasmi::{ModuleRef, NopExternals, RuntimeValue, ExternVal, ValueType};
 use wasmi::nan_preserving_float::{F32, F64};
+use wasmi::{ExternVal, ModuleRef, NopExternals, RuntimeValue, ValueType};
 
+use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 use pyo3::{Python, ToPyObject};
-use pyo3::prelude::*;
-
 
 #[pyclass]
 pub struct WASMInstance {
-    pub instance: ModuleRef
+    pub instance: ModuleRef,
 }
 
 #[pymethods]
@@ -21,21 +20,21 @@ impl WASMInstance {
             args = create_args(tuple, signature);
         }
 
-        let result = self.instance.invoke_export(
-            &method,
-            &args,
-            &mut NopExternals).expect("Failed to execute export");
+        let result = self
+            .instance
+            .invoke_export(&method, &args, &mut NopExternals)
+            .expect("Failed to execute export");
 
         let gil = Python::acquire_gil();
-        let py = gil.python();        
+        let py = gil.python();
 
         if let Some(result) = result {
             match result {
                 RuntimeValue::I32(i) => return Ok(i.to_object(py)),
                 RuntimeValue::I64(l) => return Ok(l.to_object(py)),
                 RuntimeValue::F32(f) => return Ok(f.to_float().to_object(py)),
-                RuntimeValue::F64(d) => return Ok(d.to_float().to_object(py))
-            }            
+                RuntimeValue::F64(d) => return Ok(d.to_float().to_object(py)),
+            }
         }
 
         Ok("hello".to_object(py))
@@ -43,12 +42,13 @@ impl WASMInstance {
 }
 
 fn create_args(args: &PyTuple, signature: &[ValueType]) -> Vec<RuntimeValue> {
-    args.iter().zip(signature).map(|(arg, &wasm_type)| {
-        match wasm_type {
+    args.iter()
+        .zip(signature)
+        .map(|(arg, &wasm_type)| match wasm_type {
             ValueType::I32 => {
                 let i32_ = i32::extract(&arg).expect("Conversion failed");
                 return RuntimeValue::from(i32_);
-            },
+            }
             ValueType::I64 => {
                 let i64_ = i64::extract(&arg).expect("Conversion failed");
                 return RuntimeValue::from(i64_);
@@ -56,13 +56,13 @@ fn create_args(args: &PyTuple, signature: &[ValueType]) -> Vec<RuntimeValue> {
             ValueType::F32 => {
                 let f32_ = f32::extract(&arg).expect("Conversion failed");
                 return RuntimeValue::from(F32::from_float(f32_));
-            },
+            }
             ValueType::F64 => {
                 let f64_ = f64::extract(&arg).expect("Conversion failed");
                 return RuntimeValue::from(F64::from_float(f64_));
             }
-        }
-    }).collect()
+        })
+        .collect()
 }
 
 fn get_params<'a>(signature: &'a Option<ExternVal>) -> &'a [ValueType] {
@@ -73,5 +73,3 @@ fn get_params<'a>(signature: &'a Option<ExternVal>) -> &'a [ValueType] {
     }
     &[]
 }
-
-
--- src/module.rs
+++ src/module.rs
@@ -1,16 +1,15 @@
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 
-use wasmi::{Module, ModuleInstance, ImportsBuilder};
-use pyo3::types::PyType;
 use pyo3::prelude::*;
+use pyo3::types::PyType;
+use wasmi::{ImportsBuilder, Module, ModuleInstance};
 
 use super::instance::WASMInstance;
 
-
 #[pyclass]
 pub struct WASMModule {
-    module: Module
+    module: Module,
 }
 
 #[pymethods]
@@ -18,14 +17,13 @@ impl WASMModule {
     #[new]
     fn __new__(obj: &PyRawObject, buffer: Vec<u8>) -> PyResult<()> {
         let module = Module::from_buffer(&buffer).expect("Failed to load wasm");
-        obj.init(|_| WASMModule{module}) 
+        obj.init(|_| WASMModule { module })
     }
-
 
     #[classmethod]
     fn from_file(_cls: &PyType, path: &str) -> PyResult<WASMModule> {
         let mut file = File::open(path).expect("Can't open file");
-        let mut content : Vec<u8> = Vec::new();
+        let mut content: Vec<u8> = Vec::new();
         file.read_to_end(&mut content).expect("Can't read file");
 
         WASMModule::from_buffer(_cls, content)
@@ -34,19 +32,14 @@ impl WASMModule {
     #[classmethod]
     fn from_buffer(_cls: &PyType, buffer: Vec<u8>) -> PyResult<WASMModule> {
         let module = Module::from_buffer(&buffer).expect("Failed to load wasm");
-        Ok(WASMModule{module})
+        Ok(WASMModule { module })
     }
 
     fn create_instance(&self) -> PyResult<WASMInstance> {
-        let instance = ModuleInstance::new(
-            &self.module, 
-            &ImportsBuilder::default()
-        )
-        .expect("Failing to create instance")
-        .assert_no_start();
+        let instance = ModuleInstance::new(&self.module, &ImportsBuilder::default())
+            .expect("Failing to create instance")
+            .assert_no_start();
 
-
-        Ok(WASMInstance{instance})
+        Ok(WASMInstance { instance })
     }
 }
-
