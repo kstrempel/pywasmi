6e6f2805c5a78a3b251b818d7b3e03b7cd28b1d7
Kai Strempel <kstrempel@gmail.com>
2018-12-09 19:05:22 +0100

    using *args in invoke

--- example/mandelbrot/pywasm.py
+++ example/mandelbrot/pywasm.py
@@ -8,7 +8,7 @@ def main():
     for x in range(0, int(heigth)):
         line = ''
         for y in range(0, int(width)):
-            line += chr(instance.invoke("calc", (-2.5 + y * (2.0/heigth), -1.0 + x*(3.5/width))))
+            line += chr(instance.invoke("calc", -2.5 + y * (2.0/heigth), -1.0 + x*(3.5/width)))
         print(line)
 
 start = datetime.now()
